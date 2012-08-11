23e26e0f4b79d9da0ecc3d54443d60ab192d4aa5
Tejas Dinkar <tejas@gja.in>
2012-08-11 14:04:33 +0530

    Foobar

--- rmate_server.py
+++ rmate_server.py
@@ -1,22 +1,51 @@
-import SocketServer
+import asyncore
+import asynchat
 import socket
 import threading
 
 
-class RMateServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+class RMateServer(asyncore.dispatcher):
     def __init__(self, sublime_plugin, connection_details = ('localhost', 52698)):
-        SocketServer.TCPServer.__init__(self, connection_details, RMateHandler)
+        asyncore.dispatcher.__init__(self)
         self.sublime_plugin = sublime_plugin
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind(connection_details)
+        self.listen(5)
 
     def run(self):
-        server_thread = threading.Thread(target=self.serve_forever)
+        # asyncore.loop()
+        server_thread = threading.Thread(target=asyncore.loop)
         server_thread.daemon = True
         server_thread.start()
 
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is None:
+            pass
+        else:
+            sock, addr = pair
+            RMateHandler(sock)
 
-class RMateHandler(SocketServer.StreamRequestHandler):
-    def say_hello(self):
-        self.wfile.write(socket.gethostname() + "\n")
+
+class RMateHandler(asyncore.dispatcher_with_send):
+    def init(self, sock):
+        asyncore.file_wrapper.__init__(self, sock)
+        self.current_line = ""
+
+    def handle_connect(self):
+        print "foobar"
+        # self.send(socket.gethostname() + "\n")
+
+    def handle_accept(self):
+        print "blah"
+
+    def handle_read(self):
+        data = self.recv(10)
+        if '\n' in data:
+            print "line"
+        else:
+            print "no line"
 
     def handle(self):
         self.say_hello()
