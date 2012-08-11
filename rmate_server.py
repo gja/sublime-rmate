93976b9d8a557a5d93e23ad9e0bb02200453f58c
Tejas Dinkar <tejas@gja.in>
2012-08-12 00:19:52 +0530

    Parsing the received data

--- rmate_server.py
+++ rmate_server.py
@@ -28,11 +28,62 @@ class RMateServer(asyncore.dispatcher):
             handler.say_hello()
 
 
+class WaitingForCommand:
+    def __init__(self, handler):
+        self.handler = handler
+        self.handler.set_terminator('\n')
+
+    def data_received(self, data):
+        return WaitingForHeaders(data, self.handler)
+
+
+class WaitingForHeaders:
+    def __init__(self, cmd, handler):
+        self.cmd = cmd
+        self.handler = handler
+        self.handler.set_terminator('\n')
+        self.headers = {}
+
+    def data_received(self, data):
+        key, value = data.split(': ', 1)
+        self.headers[key] = value
+        if key != "data":
+            return self
+        elif int(value) == 0:
+            return WaitingForDot(self.cmd, self.headers, "", self.handler)
+        else:
+            return WaitingForData(self.cmd, self.headers, self.handler)
+
+
+class WaitingForData:
+    def __init__(self, cmd, headers, handler):
+        self.cmd = cmd
+        self.headers = headers
+        self.handler = handler
+        self.handler.set_terminator(int(headers["data"]))
+
+    def data_received(self, data):
+        return WaitingForDot(self.cmd, self.headers, data, self.handler)
+
+
+class WaitingForDot:
+    def __init__(self, cmd, headers, data, handler):
+        self.cmd = cmd
+        self.headers = headers
+        self.data = data
+        self.handler = handler
+        self.handler.set_terminator("\n")
+
+    def data_received(self, data):
+        print "the data is " + self.data  # and open sublime
+        return WaitingForCommand(self.handler)
+
+
 class RMateHandler(asynchat.async_chat):
     def __init__(self, sock):
-        self.received_data = ""
         asynchat.async_chat.__init__(self, sock)
-        self.set_terminator('\n')
+        self.received_data = ""
+        self.state = WaitingForCommand(self)
 
     def say_hello(self):
         self.send(socket.gethostname() + "\n")
@@ -41,7 +92,9 @@ class RMateHandler(asynchat.async_chat):
         self.received_data += data
 
     def found_terminator(self):
-        print self.received_data
+        if self.received_data == "":
+            return
+        self.state = self.state.data_received(self.received_data)
         self.received_data = ""
 
 server = RMateServer("foobar")
